a3606a1d6bf4d82b6c0827b0fb3d7ebdd485d0f8
James <jamesthollowell@gmail.com>
2018-12-02 22:25:46 -0500

    Day 2 Part 2

--- src/day2/day2.kt
+++ src/day2/day2.kt
@@ -1,5 +1,6 @@
 package day2
 
+import utils.Distance
 import java.io.File
 
 fun main(args: Array<String>) {
@@ -14,11 +15,10 @@ fun main(args: Array<String>) {
         var hasThrees = false
         line.forEach { map.set(it, map.getOrDefault(it, 0) + 1) }
         for (value in map.values) {
-            if(!hasTwos && value == 2) {
+            if (!hasTwos && value == 2) {
                 hasTwos = true
                 twos++
-            }
-            else if (!hasThrees && value == 3) {
+            } else if (!hasThrees && value == 3) {
                 hasThrees = true
                 threes++
             }
@@ -27,4 +27,20 @@ fun main(args: Array<String>) {
     print("Day 1: ")
     println(twos * threes)
 
+
+    print("Day 2: ")
+    val distanceUtil = Distance()
+    outer@ for ((ndx, word) in input.withIndex()) {
+        for (word2 in input.subList(ndx, input.size)) {
+            if(distanceUtil.LD(word, word2) == 1) {
+                for ((i, c) in word.withIndex() ) {
+                    if (c == word2.toCharArray()[i]) {
+                        print(c)
+                    }
+                }
+                println()
+                break@outer
+            }
+        }
+    }
 }
